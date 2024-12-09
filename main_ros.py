b4255459636ed6021569a09902f0a784921cb03c
sachaMorin <morin.sacha@gmail.com>
2024-12-09 14:39:53 -0500

    CLIP query service

--- main_ros.py
+++ main_ros.py
@@ -1,3 +1,5 @@
+from typing import List
+
 import datetime
 import os
 import time
@@ -9,6 +11,7 @@ import hydra
 from omegaconf import DictConfig, OmegaConf
 from tqdm import tqdm
 import numpy as np
+import open3d as o3d
 
 import rclpy
 from rclpy.node import Node
@@ -21,10 +24,12 @@ from tf2_ros.buffer import Buffer
 from tf2_ros.transform_listener import TransformListener
 
 
-from ovmm_ros_msg.msg import RGBDImage
 from ovmm_ros.utils.rgbd import decode_RGBD_msg
+from ovmm_ros_msg.msg import RGBDImage
+from ovmm_ros_msg.srv import CLIPRetrieval
 
 from concept_graphs.utils import set_seed
+from concept_graphs.viz.utils import similarities_to_rgb
 from concept_graphs.mapping.utils import test_unique_segments
 
 from sensor_msgs.msg import PointCloud2
@@ -47,7 +52,7 @@ def point_cloud_msg(points, colors, parent_frame):
     # represents the x-coordinate, the next 4 the y-coordinate, etc.
     fields = [PointField(
         name=n, offset=i*itemsize, datatype=ros_dtype, count=1)
-        for i, n in enumerate('xyzrgb')]
+        for i, n in enumerate('xyzbgr')]
 
     # The PointCloud2 message also has a header which specifies which 
     # coordinate frame it is represented in. 
@@ -65,6 +70,17 @@ def point_cloud_msg(points, colors, parent_frame):
         data=data
     )
 
+def broadcast_color_pcd(pcds_o3d: List[o3d.geometry.PointCloud], colors: np.array):
+    """Take a list with n_objects pcds, array of size (n_objects, 3) and returns an array of size (n_points, 3)."""
+    pcd_points = [np.asarray(p.points) for p in pcds_o3d]
+    pcd_colors = []
+    for i, p in enumerate(pcd_points):
+        c = colors[i].reshape((1, 3))
+        c = np.tile(c, (len(p), 1))
+        pcd_colors.append(c)
+    pcd_colors = np.concatenate(pcd_colors)
+
+    return pcd_colors
 
 
 class SceneGraphNode(Node):
@@ -101,17 +117,31 @@ class SceneGraphNode(Node):
         self.n_segments = 0
         self.get_logger().info("Scene Graph ROS Node is up!")
 
+        # Query service
+        self.clip_query_service = self.create_service(CLIPRetrieval, "concept_graphs/clip_query", self.clip_query_service_callback)
+
+        # Filter service
+        
 
         # Point cloud publishers
         self.pcd_rgb_publisher = self.create_publisher(PointCloud2, 'concept_graphs/point_cloud/rgb', 10)
         self.pcd_instance_publisher = self.create_publisher(PointCloud2, 'concept_graphs/point_cloud/instance', 10)
+        self.pcd_similarity_publisher = self.create_publisher(PointCloud2, 'concept_graphs/point_cloud/similarity', 10)
+        self.pcd_query_publisher = self.create_publisher(PointCloud2, 'concept_graphs/point_cloud/query', 10)
+
         timer_period = 1.0  # seconds
         self.pcd_instance_timer = self.create_timer(timer_period, self.pcd_instance_callback)
         self.pcd_rgb_timer = self.create_timer(timer_period, self.pcd_rgb_callback)
+        self.pcd_similarity_timer = self.create_timer(timer_period, self.pcd_similarity_callback)
+        self.pcd_query_timer = self.create_timer(timer_period, self.pcd_query_callback)
+
         self.pcds_o3d = None
         self.pcd_points = None
         self.pcd_rgb = None
         self.pcd_instance = None
+        self.pcd_similarity = None
+        self.pcd_query_points = None
+        self.pcd_query_rgb = None
         self.random_colors = np.random.rand(10000, 3)
 
 
@@ -138,16 +168,43 @@ class SceneGraphNode(Node):
 
         pcd_points = [np.asarray(p.points) for p in self.pcds_o3d]
         self.pcd_points = np.concatenate(pcd_points)
+
         pcd_rgb = [np.asarray(p.colors) for p in self.pcds_o3d]
         self.pcd_rgb = np.concatenate(pcd_rgb)
-        self.pcd_rgb = self.pcd_rgb[:, [2, 1, 0]] # BGR
 
-        pcd_instance = []
-        for i, p in enumerate(pcd_points):
-            c = self.random_colors[i].reshape((1, 3))
-            c = np.tile(c, (len(p), 1))
-            pcd_instance.append(c)
-        self.pcd_instance = np.concatenate(pcd_instance)
+        self.pcd_instance = broadcast_color_pcd(self.pcds_o3d, self.random_colors)
+
+
+    def clip_query_service_callback(self, request, response):
+        if not len(self.main_map):
+            response.success = False
+            return response
+
+        # Query CLIP similarities
+        query_ft = self.perception_pipeline.ft_extractor.encode_text([request.query])
+        sim_objects = self.main_map.similarity.semantic_similarity(query_ft, self.main_map.semantic_tensor)
+        sim_objects = sim_objects.squeeze().cpu().numpy().astype(float)
+
+        best_match_idx = sim_objects.argmax()
+
+        sim_rgb = np.array(similarities_to_rgb(sim_objects, "viridis")) / 255
+
+        self.pcd_similarity = broadcast_color_pcd(self.pcds_o3d, sim_rgb)
+        self.pcd_similarity = self.pcd_similarity[:, [2, 1, 0]]
+
+        # Get most similar object
+        self.pcd_query_points = np.array(self.pcds_o3d[best_match_idx].points)
+        self.pcd_query_rgb = np.array(self.pcds_o3d[best_match_idx].colors)
+        translation = self.pcd_query_points.mean(axis=0).astype(float)
+
+        response.success = True
+        response.frame_id = self.tf_frame
+        response.x = translation[0] 
+        response.y = translation[1]
+        response.z = translation[2]
+        response.similarity = sim_objects[best_match_idx]
+
+        return response
 
     def pcd_instance_callback(self):
         if self.pcds_o3d is None:
@@ -162,6 +219,20 @@ class SceneGraphNode(Node):
 
         pcd_msg = point_cloud_msg(self.pcd_points, self.pcd_rgb, self.tf_frame)
         self.pcd_rgb_publisher.publish(pcd_msg)
+
+    def pcd_similarity_callback(self):
+        if self.pcd_similarity is None or len(self.pcd_similarity) != len(self.pcd_points):
+            return
+
+        pcd_msg = point_cloud_msg(self.pcd_points, self.pcd_similarity, self.tf_frame)
+        self.pcd_similarity_publisher.publish(pcd_msg)
+
+    def pcd_query_callback(self):
+        if self.pcd_query_points is None or self.pcd_query_rgb is None:
+            return
+
+        pcd_msg = point_cloud_msg(self.pcd_query_points, self.pcd_query_rgb, self.tf_frame)
+        self.pcd_query_publisher.publish(pcd_msg)
 
 
 @hydra.main(version_base=None, config_path="conf", config_name="main_ros")
